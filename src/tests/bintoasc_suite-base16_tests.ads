26f10f0fdb94afe3d55a6356fe6357d0ea0eea42
James Humphry <git@binecho.co.uk>
2015-09-28 22:39:48 +0100

    Refactor unit tests: common test vector routines
    As all the codecs are part of a common object-oriented hierarchy, it is
    possible to factor out the unit tests relating to known test vectors.
    The unit tests for each codec now only have to instantiate these tests
    for the given Codec_To_String'Class, Codec_To_Bin'Class and
    Test_Vector_Array, thus preventing repetitious boilerplate tests.

--- src/tests/bintoasc_suite-base16_tests.ads
+++ src/tests/bintoasc_suite-base16_tests.ads
@@ -6,15 +6,19 @@
 with AUnit; use AUnit;
 with AUnit.Test_Cases; use AUnit.Test_Cases;
 
+with RFC4648;
+
+with BinToAsc_Suite.Utils;
+
 package BinToAsc_Suite.Base16_Tests is
 
-   Base16_Test_Vectors : Test_Vector_Array := ((TBS(""), TBS("")),
-                                               (TBS("f"), TBS("66")),
-                                               (TBS("fo"), TBS("666F")),
-                                               (TBS("foo"), TBS("666F6F")),
-                                               (TBS("foob"), TBS("666F6F62")),
-                                               (TBS("fooba"), TBS("666F6F6261")),
-                                               (TBS("foobar"), TBS("666F6F626172")));
+   Base16_Test_Vectors : constant Test_Vector_Array := ((TBS(""), TBS("")),
+                                                        (TBS("f"), TBS("66")),
+                                                        (TBS("fo"), TBS("666F")),
+                                                        (TBS("foo"), TBS("666F6F")),
+                                                        (TBS("foob"), TBS("666F6F62")),
+                                                        (TBS("fooba"), TBS("666F6F6261")),
+                                                        (TBS("foobar"), TBS("666F6F626172")));
 
    type Base16_Test is new Test_Cases.Test_Case with null record;
 
@@ -24,8 +28,35 @@ package BinToAsc_Suite.Base16_Tests is
 
    procedure Set_Up (T : in out Base16_Test);
 
-   procedure Check_Test_Vectors (T : in out Test_Cases.Test_Case'Class);
-   procedure Check_Test_Vectors_Incremental (T : in out Test_Cases.Test_Case'Class);
-   procedure Check_Test_Vectors_By_Char (T : in out Test_Cases.Test_Case'Class);
+   procedure Check_Test_Vectors_To_String is
+     new BinToAsc_Suite.Utils.Check_Test_Vectors_To_String(Test_Vectors => Base16_Test_Vectors,
+                                                           Codec_To_String => RFC4648.Base16.Base16_To_String,
+                                                           Max_Buffer_Length => 12);
+
+   procedure Check_Test_Vectors_To_Bin is
+     new BinToAsc_Suite.Utils.Check_Test_Vectors_To_Bin(Test_Vectors => Base16_Test_Vectors,
+                                                        Codec_To_Bin => RFC4648.Base16.Base16_To_Bin,
+                                                        Max_Buffer_Length => 12);
+
+
+   procedure Check_Test_Vectors_Incremental_To_String is
+     new BinToAsc_Suite.Utils.Check_Test_Vectors_Incremental_To_String(Test_Vectors => Base16_Test_Vectors,
+                                                                       Codec_To_String => RFC4648.Base16.Base16_To_String,
+                                                                       Max_Buffer_Length => 12);
+
+   procedure Check_Test_Vectors_Incremental_To_Bin is
+     new BinToAsc_Suite.Utils.Check_Test_Vectors_Incremental_To_Bin(Test_Vectors => Base16_Test_Vectors,
+                                                                    Codec_To_Bin => RFC4648.Base16.Base16_To_Bin,
+                                                                    Max_Buffer_Length => 12);
+
+   procedure Check_Test_Vectors_By_Char_To_String is
+     new BinToAsc_Suite.Utils.Check_Test_Vectors_By_Char_To_String(Test_Vectors => Base16_Test_Vectors,
+                                                                   Codec_To_String => RFC4648.Base16.Base16_To_String,
+                                                                   Max_Buffer_Length => 12);
+
+   procedure Check_Test_Vectors_By_Char_To_Bin is
+     new BinToAsc_Suite.Utils.Check_Test_Vectors_By_Char_To_Bin(Test_Vectors => Base16_Test_Vectors,
+                                                                Codec_To_Bin => RFC4648.Base16.Base16_To_Bin,
+                                                                Max_Buffer_Length => 12);
 
 end BinToAsc_Suite.Base16_Tests;
---
+++ src/tests/bintoasc_suite-utils.adb
@@ -0,0 +1,340 @@
+-- BinToAsc_Suite.Utils
+-- Unit test utilities for BinToAsc
+
+-- Copyright (c) 2015, James Humphry - see LICENSE file for details
+
+with System.Storage_Elements;
+with AUnit.Assertions; use AUnit.Assertions;
+
+with String_To_Storage_Array, Storage_Array_To_String;
+
+package body BinToAsc_Suite.Utils is
+
+   use RFC4648;
+   use type RFC4648.Codec_State;
+   use System.Storage_Elements;
+
+   function STSA (X : String) return Storage_Array
+                  renames String_To_Storage_Array;
+
+   function SATS (X : Storage_Array) return String
+                  renames Storage_Array_To_String;
+
+   ----------------------------------
+   -- Check_Test_Vectors_To_String --
+   ----------------------------------
+
+   procedure Check_Test_Vectors_To_String (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+
+      BinToAsc_Encoder : Codec_To_String;
+
+      Result_String : String(1..Max_Buffer_Length);
+      Result_String_Length : Integer;
+
+   begin
+      for T of Test_Vectors loop
+         declare
+            Unencoded : constant String := To_String(T.Unencoded);
+            Encoded : constant String := To_String(T.Encoded);
+         begin
+            BinToAsc_Encoder.Reset;
+            BinToAsc_Encoder.Process(Input => STSA(Unencoded),
+                                     Output => Result_String,
+                                     Output_Length => Result_String_Length);
+            Assert(Result_String(1..Result_String_Length) = Encoded,
+                   "BinToAsc encoder on: " &
+                     Unencoded &
+                     " gives wrong result: " &
+                     Result_String(1..Result_String_Length) &
+                     " instead of: " &
+                     Encoded);
+
+            BinToAsc_Encoder.Completed(Output => Result_String,
+                                     Output_Length => Result_String_Length);
+            Assert(Result_String_Length = 0 and BinToAsc_Encoder.State = Complete,
+                   "BinToAsc encoder not terminating correctly.");
+         end;
+      end loop;
+   end Check_Test_Vectors_To_String;
+
+   -------------------------------
+   -- Check_Test_Vectors_To_Bin --
+   -------------------------------
+
+   procedure Check_Test_Vectors_To_Bin (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+
+      BinToAsc_Decoder : Codec_To_Bin;
+
+      Result_Bin : Storage_Array(1..Storage_Offset(Max_Buffer_Length));
+      Result_Bin_Length : Storage_Offset;
+
+   begin
+      for T of Test_Vectors loop
+         declare
+            Unencoded : constant String := To_String(T.Unencoded);
+            Encoded : constant String := To_String(T.Encoded);
+         begin
+            BinToAsc_Decoder.Reset;
+            BinToAsc_Decoder.Process(Input => Encoded,
+                                   Output => Result_Bin,
+                                   Output_Length => Result_Bin_Length);
+            Assert(SATS(Result_Bin(1..Result_Bin_Length)) = Unencoded,
+                   "BinToAsc decoder on: " &
+                     Encoded &
+                     " gives wrong result: " &
+                     SATS(Result_Bin(1..Result_Bin_Length)) &
+                     " instead of: " &
+                     Unencoded);
+
+            BinToAsc_Decoder.Completed(Output => Result_Bin,
+                                     Output_Length => Result_Bin_Length);
+            Assert(Result_Bin_Length = 0 and BinToAsc_Decoder.State = Complete,
+                   "BinToAsc decoder not terminating correctly.");
+         end;
+      end loop;
+   end Check_Test_Vectors_To_Bin;
+
+   ------------------------------------
+   -- Check_Test_Vectors_Incremental --
+   ------------------------------------
+
+   procedure Check_Test_Vectors_Incremental_To_String (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+
+      BinToAsc_Encoder : Codec_To_String;
+
+      Result_String : String(1..Max_Buffer_Length);
+      Result_String_Length : Integer;
+
+   begin
+      for T of Test_Vectors loop
+         declare
+            Unencoded : constant String := To_String(T.Unencoded);
+            Encoded : constant String := To_String(T.Encoded);
+
+            Buffer_String : String(1..Max_Buffer_Length);
+            Buffer_String_Used : Integer;
+
+         begin
+            BinToAsc_Encoder.Reset;
+
+            Result_String := (others => 'z');
+            Result_String_Length := 0;
+
+            for C of Unencoded loop
+               BinToAsc_Encoder.Process(Input => STSA(C & ""),
+                                        Output => Buffer_String,
+                                        Output_Length => Buffer_String_Used);
+               if Buffer_String_Used > 0 then
+                  Result_String(Result_String_Length + 1 .. Result_String_Length + Buffer_String_Used) :=
+                    Buffer_String(1..Buffer_String_Used);
+                  Result_String_Length := Result_String_Length + Buffer_String_Used;
+               end if;
+            end loop;
+
+            BinToAsc_Encoder.Completed(Output => Buffer_String,
+                                       Output_Length => Buffer_String_Used);
+            if Buffer_String_Used > 0 then
+               Result_String(Result_String_Length + 1 .. Result_String_Length + Buffer_String_Used) :=
+                 Buffer_String(1..Buffer_String_Used);
+               Result_String_Length := Result_String_Length + Buffer_String_Used;
+            end if;
+
+            Assert(BinToAsc_Encoder.State = Complete,
+                   "BinToAsc encoder not terminating correctly.");
+
+            Assert(Result_String(1..Result_String_Length) = Encoded,
+                   "BinToAsc encoder on: " &
+                     Unencoded &
+                     " gives wrong result: " &
+                     Result_String(1..Result_String_Length) &
+                     " instead of: " &
+                     Encoded);
+         end;
+      end loop;
+   end Check_Test_Vectors_Incremental_To_String;
+
+   -------------------------------------------
+   -- Check_Test_Vectors_Incremental_To_Bin --
+   -------------------------------------------
+
+   procedure Check_Test_Vectors_Incremental_To_Bin (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+
+      BinToAsc_Decoder : Codec_To_Bin;
+
+      Result_Bin : Storage_Array(1..Storage_Offset(Max_Buffer_Length));
+      Result_Bin_Length : Storage_Offset;
+
+   begin
+      for T of Test_Vectors loop
+         declare
+            Unencoded : constant String := To_String(T.Unencoded);
+            Encoded : constant String := To_String(T.Encoded);
+
+            Buffer_Bin : Storage_Array(1..Storage_Offset(Max_Buffer_Length));
+            Buffer_Bin_Used : Storage_Offset;
+
+         begin
+
+            BinToAsc_Decoder.Reset;
+
+            Result_Bin := (others => 0);
+            Result_Bin_Length := 0;
+
+            for C of Encoded loop
+               BinToAsc_Decoder.Process(Input => C & "",
+                                        Output => Buffer_Bin,
+                                        Output_Length => Buffer_Bin_Used);
+               if Buffer_Bin_Used > 0 then
+                  Result_Bin(Result_Bin_Length + 1 .. Result_Bin_Length + Buffer_Bin_Used) :=
+                    Buffer_Bin(1..Buffer_Bin_Used);
+                  Result_Bin_Length := Result_Bin_Length + Buffer_Bin_Used;
+               end if;
+            end loop;
+
+            BinToAsc_Decoder.Completed(Output => Buffer_Bin,
+                                       Output_Length => Buffer_Bin_Used);
+            if Buffer_Bin_Used > 0 then
+               Result_Bin(Result_Bin_Length + 1 .. Result_Bin_Length + Buffer_Bin_Used) :=
+                 Buffer_Bin(1..Buffer_Bin_Used);
+               Result_Bin_Length := Result_Bin_Length + Buffer_Bin_Used;
+            end if;
+
+            Assert(BinToAsc_Decoder.State = Complete,
+                   "BinToAsc decoder not terminating correctly.");
+
+            Assert(SATS(Result_Bin(1..Result_Bin_Length)) = Unencoded,
+                   "BinToAsc decoder on: " &
+                     Encoded &
+                     " gives wrong result: " &
+                     SATS(Result_Bin(1..Result_Bin_Length)) &
+                     " instead of: " &
+                     Unencoded);
+         end;
+      end loop;
+   end Check_Test_Vectors_Incremental_To_Bin;
+
+   ------------------------------------------
+   -- Check_Test_Vectors_By_Char_To_String --
+   ------------------------------------------
+
+   procedure Check_Test_Vectors_By_Char_To_String (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+
+      BinToAsc_Encoder : Codec_To_String;
+
+      Result_String : String(1..Max_Buffer_Length);
+      Result_String_Length : Integer;
+
+   begin
+      for T of Test_Vectors loop
+         declare
+            Unencoded : constant String := To_String(T.Unencoded);
+            Encoded : constant String := To_String(T.Encoded);
+
+            Buffer_String : String(1..Max_Buffer_Length);
+            Buffer_String_Used : Integer;
+
+         begin
+            BinToAsc_Encoder.Reset;
+
+            Result_String := (others => 'z');
+            Result_String_Length := 0;
+
+            for C of Unencoded loop
+               BinToAsc_Encoder.Process(Input => Storage_Element'Val(Character'Pos(C)),
+                                      Output => Buffer_String,
+                                      Output_Length => Buffer_String_Used);
+               if Buffer_String_Used > 0 then
+                  Result_String(Result_String_Length + 1 .. Result_String_Length + Buffer_String_Used) :=
+                    Buffer_String(1..Buffer_String_Used);
+                  Result_String_Length := Result_String_Length + Buffer_String_Used;
+               end if;
+            end loop;
+
+            BinToAsc_Encoder.Completed(Output => Buffer_String,
+                                     Output_Length => Buffer_String_Used);
+            if Buffer_String_Used > 0 then
+               Result_String(Result_String_Length + 1 .. Result_String_Length + Buffer_String_Used) :=
+                 Buffer_String(1..Buffer_String_Used);
+               Result_String_Length := Result_String_Length + Buffer_String_Used;
+            end if;
+
+            Assert(BinToAsc_Encoder.State = Complete,
+                   "BinToAsc encoder not terminating correctly.");
+
+            Assert(Result_String(1..Result_String_Length) = Encoded,
+                   "BinToAsc encoder on: " &
+                     Unencoded &
+                     " gives wrong result: " &
+                     Result_String(1..Result_String_Length) &
+                     " instead of: " &
+                     Encoded);
+         end;
+      end loop;
+   end Check_Test_Vectors_By_Char_To_String;
+
+   ---------------------------------------
+   -- Check_Test_Vectors_By_Char_To_Bin --
+   ---------------------------------------
+
+   procedure Check_Test_Vectors_By_Char_To_Bin (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
+
+      BinToAsc_Decoder : Codec_To_Bin;
+
+      Result_Bin : Storage_Array(1..Storage_Offset(Max_Buffer_Length));
+      Result_Bin_Length : Storage_Offset;
+
+   begin
+      for T of Test_Vectors loop
+         declare
+            Unencoded : constant String := To_String(T.Unencoded);
+            Encoded : constant String := To_String(T.Encoded);
+
+            Buffer_Bin : Storage_Array(1..Storage_Offset(Max_Buffer_Length));
+            Buffer_Bin_Used : Storage_Offset;
+
+         begin
+            BinToAsc_Decoder.Reset;
+
+            Result_Bin := (others => 0);
+            Result_Bin_Length := 0;
+
+            for C of Encoded loop
+               BinToAsc_Decoder.Process(Input => C,
+                                      Output => Buffer_Bin,
+                                      Output_Length => Buffer_Bin_Used);
+               if Buffer_Bin_Used > 0 then
+                  Result_Bin(Result_Bin_Length + 1 .. Result_Bin_Length + Buffer_Bin_Used) :=
+                    Buffer_Bin(1..Buffer_Bin_Used);
+                  Result_Bin_Length := Result_Bin_Length + Buffer_Bin_Used;
+               end if;
+            end loop;
+
+            BinToAsc_Decoder.Completed(Output => Buffer_Bin,
+                                     Output_Length => Buffer_Bin_Used);
+            if Buffer_Bin_Used > 0 then
+               Result_Bin(Result_Bin_Length + 1 .. Result_Bin_Length + Buffer_Bin_Used) :=
+                 Buffer_Bin(1..Buffer_Bin_Used);
+               Result_Bin_Length := Result_Bin_Length + Buffer_Bin_Used;
+            end if;
+
+            Assert(BinToAsc_Decoder.State = Complete,
+                   "BinToAsc decoder not terminating correctly.");
+
+            Assert(SATS(Result_Bin(1..Result_Bin_Length)) = Unencoded,
+                   "BinToAsc decoder on: " &
+                     Encoded &
+                     " gives wrong result: " &
+                     SATS(Result_Bin(1..Result_Bin_Length)) &
+                     " instead of: " &
+                     Unencoded);
+         end;
+      end loop;
+   end Check_Test_Vectors_By_Char_To_Bin;
+
+end BinToAsc_Suite.Utils;
