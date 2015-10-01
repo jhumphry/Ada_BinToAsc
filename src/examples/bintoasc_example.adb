34cc3b2253d8c3e4d06994e57ac16116a11ce4ff
James Humphry <git@binecho.co.uk>
2015-10-01 19:16:20 +0100

    Add Base32Hex to example program and tidy up

--- src/examples/bintoasc_example.adb
+++ src/examples/bintoasc_example.adb
@@ -17,50 +17,64 @@ begin
    Put_Line("Examples of using the BinToAsc package.");
    New_Line;
 
-   -- Demonstrate coding into BASE16
-   Put_Line("According to RFC4648 BASE16('foobar') = '666F6F626172'");
-   Put("According to this package BASE16('foobar') = '");
+   -- Demonstrate coding into Base16
+   Put_Line("According to RFC4648 Base16('foobar') = '666F6F626172'");
+   Put("According to this package Base16('foobar') = '");
    Put(Base16.To_String(String_To_Storage_Array("foobar")));
    Put_Line("'");
    New_Line;
 
-   -- Demonstrate decoding from BASE16
-   Put_Line("According to RFC4648 BASE16^{-1}('666F6F626172') = 'foobar'");
-   Put("According to this package BASE16^{-1}('666F6F626172') = '");
+   -- Demonstrate decoding from Base16
+   Put_Line("According to RFC4648 Base16^{-1}('666F6F626172') = 'foobar'");
+   Put("According to this package Base16^{-1}('666F6F626172') = '");
    Put(Storage_Array_To_String(Base16.To_Bin("666F6F626172")));
    Put_Line("'");
    New_Line;
 
-   -- Demonstrate coding into BASE32
-   Put_Line("According to RFC4648 BASE32('foobar') = 'MZXW6YTBOI======'");
-   Put("According to this package BASE32('foobar') = '");
+   -- Demonstrate coding into Base32
+   Put_Line("According to RFC4648 Base32('foobar') = 'MZXW6YTBOI======'");
+   Put("According to this package Base32('foobar') = '");
    Put(Base32.To_String(String_To_Storage_Array("foobar")));
    Put_Line("'");
 
    New_Line;
-   -- Demonstrate decoding from BASE32
-   Put_Line("According to RFC4648 BASE32^{-1}('MZXW6YTBOI======') = 'foobar'");
-   Put("According to this package BASE32^{-1}('MZXW6YTBOI======') = '");
+   -- Demonstrate decoding from Base32
+   Put_Line("According to RFC4648 Base32^{-1}('MZXW6YTBOI======') = 'foobar'");
+   Put("According to this package Base32^{-1}('MZXW6YTBOI======') = '");
    Put(Storage_Array_To_String(Base32.To_Bin("MZXW6YTBOI======")));
    Put_Line("'");
    New_Line;
 
-   -- Demonstrate coding into BASE64
-   Put_Line("According to RFC4648 BASE64('foobar') = 'Zm9vYmFy'");
-   Put("According to this package BASE16('foobar') = '");
+   -- Demonstrate coding into Base32Hex
+   Put_Line("According to RFC4648 Base32Hex('foobar') = 'CPNMUOJ1E8======'");
+   Put("According to this package Base32Hex('foobar') = '");
+   Put(Base32Hex.To_String(String_To_Storage_Array("foobar")));
+   Put_Line("'");
+
+   New_Line;
+   -- Demonstrate decoding from Base32Hex
+   Put_Line("According to RFC4648 Base32Hex^{-1}('CPNMUOJ1E8======') = 'foobar'");
+   Put("According to this package Base32Hex^{-1}('CPNMUOJ1E8======') = '");
+   Put(Storage_Array_To_String(Base32Hex.To_Bin("CPNMUOJ1E8======")));
+   Put_Line("'");
+   New_Line;
+
+   -- Demonstrate coding into Base64
+   Put_Line("According to RFC4648 Base64('foobar') = 'Zm9vYmFy'");
+   Put("According to this package Base16('foobar') = '");
    Put(Base64.To_String(String_To_Storage_Array("foobar")));
    Put_Line("'");
    New_Line;
 
-   Put_Line("According to RFC4648 BASE64('foob') = 'Zm9vYg=='");
-   Put("According to this package BASE16('foob') = '");
+   Put_Line("According to RFC4648 Base64('foob') = 'Zm9vYg=='");
+   Put("According to this package Base16('foob') = '");
    Put(Base64.To_String(String_To_Storage_Array("foob")));
    Put_Line("'");
    New_Line;
 
-   -- Demonstrate decoding from BASE64
-   Put_Line("According to RFC4648 BASE64^{-1}('Zm8=') = 'fo'");
-   Put("According to this package BASE64^{-1}('Zm8=') = '");
+   -- Demonstrate decoding from Base64
+   Put_Line("According to RFC4648 Base64^{-1}('Zm8=') = 'fo'");
+   Put("According to this package Base64^{-1}('Zm8=') = '");
    Put(Storage_Array_To_String(Base64.To_Bin("Zm8=")));
    Put_Line("'");
    New_Line;
